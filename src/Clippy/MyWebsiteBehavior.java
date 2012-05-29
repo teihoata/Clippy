bd91b8834c7e1d730d203333c9b6f2d268f26506
Marcus Ball <teihoata@gmail.com>
2012-05-30 00:05:32 +1200

    Added some changes from Gavin and me - Now has a dedicated drop down menu for removing websites - List populates with what is in the database and allows the user to easily remove websites by clicking delete

--- src/Clippy/MyWebsiteBehavior.java
+++ src/Clippy/MyWebsiteBehavior.java
@@ -4,6 +4,7 @@
 package Clippy;
 
 import ClippyV2.ui.View.ClippyGui;
+import ClippyV2.ui.View.WebMenu;
 import com.sun.speech.engine.recognition.BaseRecognizer;
 import com.sun.speech.engine.recognition.BaseRuleGrammar;
 import com.sun.speech.freetts.Voice;
@@ -26,6 +27,7 @@ import javax.swing.JOptionPane;
 /**
  *
  * @author Marcus Ball
+ * @edited by Gavin Chan & Rayleigh Zhang
  */
 public class MyWebsiteBehavior extends MyBehavior
 {
@@ -33,6 +35,7 @@ public class MyWebsiteBehavior extends MyBehavior
     ArrayList<String> websiteList; //holds the names of current websites
     private int count; //number of rules within the dialog
     private ArrayList<String> menuList;
+    private boolean processed;
     // added by Ray
     private DBOperator dbo = new DBOperator();
 
@@ -100,128 +103,166 @@ public class MyWebsiteBehavior extends MyBehavior
         this.setList(menuList);
     }
 
-    /**
-     * Adds the grammar rule to the list of dynamic grammars for desktop
-     * behavior
-     *
+   /**
+     * Adds the grammar rule to the list of dynamic grammars for desktop behavior
      * @param ruleGrammar
      * @param ruleName
-     * @param grammarName
+     * @param grammarName 
      */
     private void addGrammar(RuleGrammar ruleGrammar, String ruleName, String grammarName)
     {
-        try
-        {
+        try {
             String newRuleName = ruleName + count;
             Rule newRule = null;
             newRule = ruleGrammar.ruleForJSGF(grammarName);
             ruleGrammar.setRule(newRuleName, newRule, true);
             ruleGrammar.setEnabled(newRuleName, true);
             count++;
-        } catch (GrammarException ex)
-        {
+        } catch (GrammarException ex) {
             System.out.println("Trouble with the grammar ");
         }
     }
-
+    
     /**
      * sends the command to be executed by ClippyAlpha executable
-     *
-     * @param command
+     * @param command 
      */
     private void sendCommand(String command)
     {
-        try
-        {
+        try {
             Process process = new ProcessBuilder("./Windows Control/ClippyAlpha2.exe", command).start();
-        } catch (IOException ex)
+        } catch (IOException ex) 
         {
             System.out.println("Couldn't find ClippyAlpha2.exe in Windows Control in root");
         }
     }
-
+    
+    /**
+     * Processes the result of the WebSite command of the user 
+     * @param result the users command 
+     * @return true if the process is successful otherwise false
+     */
+    @Override
     public boolean processResult(String result)
     {
-        boolean processed = false;
+        processed = false;
         List<String> websites = dbo.getWebSites();
-        if (result.equalsIgnoreCase("remove website"))
+        if(result.equalsIgnoreCase("remove website"))
+         {
+             removeWebsite(websites);    
+         }
+        
+        else if(result.equalsIgnoreCase("add new website"))
         {
-            processed = true;
-        }
-        else if (result.equalsIgnoreCase("add new website"))
-        {
-            try
-            {
-                String websiteName = (String) JOptionPane.showInputDialog(null, "Enter the address of your website starting with 'www.'", "Add Website", JOptionPane.INFORMATION_MESSAGE, null, null, "www.");
-                if (websiteName != null || !websiteName.isEmpty() || !websiteName.equalsIgnoreCase("www."))
-                {
-                    websites.add(websiteName);
-                    dbo.removeAllWebsite();
-                    dbo.storeWebSite(websites);
-                    onEntry();
-                    processed = true;
-                    help();
-                }
-            } catch (JSGFGrammarParseException ex)
-            {
-                Logger.getLogger(MyWebsiteBehavior.class.getName()).log(Level.SEVERE, null, ex);
-            } catch (JSGFGrammarException ex)
-            {
-                Logger.getLogger(MyWebsiteBehavior.class.getName()).log(Level.SEVERE, null, ex);
-            } catch (IOException ex)
-            {
-                Logger.getLogger(MyWebsiteBehavior.class.getName()).log(Level.SEVERE, null, ex);
-            }
+            addWebsite(websites);
         }
         else if (result.startsWith("open"))
         {
-            String url = "";
-            String nameOfCurrentWebsite = "";
-            String detail = "";
-            Iterator<String> it = websites.iterator();
-            while (it.hasNext())
-            {
-                detail = it.next();
-                String currentWebsite = detail;
-
-                try
-                {
-                    detail = detail.substring(detail.indexOf(".") + 1, detail.indexOf(".", detail.indexOf(".") + 1));
-
-                    if (result.substring(result.indexOf("open") + 5).equalsIgnoreCase(detail))
-                    {
-                        url = currentWebsite;
-                        nameOfCurrentWebsite = detail;
-                    }
-                } catch (StringIndexOutOfBoundsException e)
-                {
-                }
-                websiteList.add(detail);
-            }
-            try
-            {
-                java.awt.Desktop.getDesktop().browse(java.net.URI.create(url));
-                String voiceName = "kevin16";
-                VoiceManager voiceManager = VoiceManager.getInstance();
-                Voice voice = voiceManager.getVoice(voiceName);
-                voice.allocate();
-                voice.speak("opened " + nameOfCurrentWebsite);
-                voice.deallocate();
-            } catch (IOException ex)
-            {
-                System.out.println("Couldn't find ClippyAlpha2.exe in Windows Control in root");
-            }
-            processed = true;
+            openWebsite(result,websites);
         }
-        return processed;
+         return processed;
+    }
+    
+    /**
+     * 
+     */
+    public void removeWebsiteFromList(String website)
+    {
+                dbo.removeWebsite(website);
+                try {
+                    onEntry();
+                } 
+                catch (IOException | JSGFGrammarParseException | JSGFGrammarException ex) {
+                    Logger.getLogger(MyWebsiteBehavior.class.getName()).log(Level.SEVERE, null, ex);
+                }
+    }
+    
+    /**
+     * Creates an input dialog that displays the saved WebSites from the database 
+     * and allows the user to remove a WebSite from the database by inputting the link 
+     * @param websites the WebSite database  
+     */
+    public void removeWebsite(List websites){
+        
+        WebMenu web = new WebMenu(websites, this);
+        web.pack();
+        web.setVisible(true);
+        processed = true;
+    }
+    
+    /**
+     * Creates an input dialog that allows the user to add a WebSite to the database by inputting the link 
+     * @param websites the WebSite database  
+     */
+    public void addWebsite(List websites)
+    {
+        try {
+            String websiteName = (String) JOptionPane.showInputDialog(null, "Enter the address of your website starting with 'www.'", "Add Website", JOptionPane.INFORMATION_MESSAGE, null, null, "www.");
+            if(websiteName != null || !websiteName.isEmpty() || !websiteName.equalsIgnoreCase("www."))
+            {
+                websites.add(websiteName);
+                dbo.removeAllWebsite();
+                dbo.storeWebSite(websites);
+                onEntry();
+                processed = true;
+                help();
+            }  
+        } 
+        catch (JSGFGrammarParseException | JSGFGrammarException | IOException | NullPointerException ex) {
+            Logger.getLogger(MyWebsiteBehavior.class.getName()).log(Level.SEVERE, null, ex);
+        }   
+    }
+    /**
+     * Searches through the WebSite database for the chosen WebSite and opens it
+     * @param result the users chosen WebSite 
+     * @param websites the WebSite database  
+     */
+    public void openWebsite(String result, List websites){
+        String url = "";
+        String nameOfCurrentWebsite = "";
+        String detail = "";
+        Iterator<String> it = websites.iterator();
+        while (it.hasNext())
+        {
+            detail = it.next();
+            String currentWebsite = detail;
+                
+            try {
+                detail = detail.substring(detail.indexOf(".")+1, detail.indexOf("." , detail.indexOf(".") + 1));
+                if(result.substring(result.indexOf("open")+5).equalsIgnoreCase(detail))
+                {
+                    url = currentWebsite;
+                    nameOfCurrentWebsite = detail;
+                }
+            }
+                
+            catch(StringIndexOutOfBoundsException e){
+            
+            }
+            websiteList.add(detail);
+        }
+        try
+        {
+            java.awt.Desktop.getDesktop().browse(java.net.URI.create(url));
+            String voiceName = "kevin16";
+            VoiceManager voiceManager = VoiceManager.getInstance();              
+            Voice voice = voiceManager.getVoice(voiceName);
+            voice.allocate();
+            voice.speak("opened " + nameOfCurrentWebsite);
+            voice.deallocate();
+        }
+        catch (IOException ex)
+        {
+            System.out.println("Couldn't find ClippyAlpha2.exe in Windows Control in root");
+        }
+        processed = true;  
     }
 
     /**
      * Called when Clippy recognises an input
-     *
      * @param result
      * @return
-     * @throws GrammarException
+     * @throws GrammarException 
      */
     @Override
     public String onRecognize(Result result) throws GrammarException
@@ -229,7 +270,7 @@ public class MyWebsiteBehavior extends MyBehavior
         String tag = super.onRecognize(result);
         String listen = result.getBestFinalResultNoFiller();
         trace("Recognize result: " + result.getBestFinalResultNoFiller());
-        if (processResult(listen))
+        if(processResult(listen))
         {
             tag = "processed";
         }
--- src/ClippyV2/ui/View/WebMenu.java
+++ src/ClippyV2/ui/View/WebMenu.java
@@ -4,43 +4,64 @@
  */
 package ClippyV2.ui.View;
 
-import java.awt.BorderLayout;
+import Clippy.MyWebsiteBehavior;
+import ClippyV2.ui.Frame;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
+import java.util.List;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
-import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 /**
  *
- * @author dns3948
+ * @author Marcus Ball
  */
-public class WebMenu extends JFrame {
+public class WebMenu extends Frame {
     private ArrayList<String> webArray = new ArrayList();
     JPanel pnl = new JPanel();
-    public WebMenu(ArrayList web){
-        webArray = web;
-        JComboBox webList = new JComboBox(webArray.toArray());
-        webList.setSelectedIndex(0);
-        pnl.add(webList, BorderLayout.CENTER);
-        this.add(pnl, BorderLayout.NORTH);
-        JPanel botPnl = new JPanel();
-        botPnl.add(new JButton("Delete"), BorderLayout.EAST);
-        botPnl.add(new JButton("Exit"), BorderLayout.WEST);
-        this.add(botPnl, BorderLayout.SOUTH);
-       // petList.addActionListener(this);   
-        
-    }
     
-     public static void main(String[] args) {
-         ArrayList<String> list = new ArrayList();
-         list.add("rar");
-         list.add("rar");
-         list.add("rar");
-         list.add("rar");
-         WebMenu web = new WebMenu(list);
-         web.setSize(240,100);
-         web.setVisible(true);
-         web.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-     }
+    public WebMenu(List<String> webArray , final MyWebsiteBehavior webBehav){
+        super(240,100,700,500); 
+        webArray.add(0, "--Select a website to remove--");
+        final JComboBox webList = new JComboBox(webArray.toArray());
+        webList.setSelectedIndex(0);
+        webList.setOpaque(true);
+        webList.setBounds(10, 30, 220, 20);
+        
+        JLabel heading = new JLabel("Remove Website");
+        heading.setBounds(10, 10, 220, 20);
+        
+        JButton deleteBtn = new JButton("Delete");
+        deleteBtn.setBounds(5, 60, 70, 30);
+        deleteBtn.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e){
+                if(webList.getSelectedIndex() != 0)
+                {
+                    System.out.println(webList.getSelectedItem());
+                    webBehav.removeWebsiteFromList(webList.getSelectedItem().toString());
+                    webList.removeItem(webList.getSelectedItem());
+                    webList.setSelectedIndex(0);
+                }
+            }
+        });
+        
+        JButton exitBtn = new JButton("Exit");
+        exitBtn.setBounds(165, 60, 70, 30);
+        exitBtn.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e){
+                dispose();
+            }
+        });
+        
+        pnl.add(webList);
+        pnl.add(heading);
+        pnl.add(deleteBtn);
+        pnl.add(exitBtn);
+        this.setPnl(pnl);  
+    }
 }
